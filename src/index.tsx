8702be103c5d459ad0cc6df3792477a323d91b00
Maxwell You <maxwell.you@mlb.com>
2021-09-02 08:46:14 -0600

    add module.hot.accept() to allow for module replacement instead of full refresh

--- src/index.tsx
+++ src/index.tsx
@@ -3,3 +3,10 @@ import React from "react";
 import ReactDom from "react-dom";
 
 ReactDom.render(<App />, document.getElementById("test-app"));
+
+// This allows for the page to just reload this component instead of the whole page
+//@ts-ignore
+if (module.hot) {
+  //@ts-ignore
+  module.hot.accept();
+}
